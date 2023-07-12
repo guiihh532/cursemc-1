944636af0f239df6800d0345c86626187dcf513e
guiihh532 <guiihh532@gmail.com>
2023-07-12 10:44:21 -0300

    Projeto criado

---
+++ src/test/java/com/gomlek/cursemc1/Cursemc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomlek.cursemc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursemc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
